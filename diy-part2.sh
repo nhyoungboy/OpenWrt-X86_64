84cbc89c04163f7380d8a07b959cce04801ac040
nhyoungboy <118877332+nhyoungboy@users.noreply.github.com>
2025-01-11 00:16:41 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,7 +9,7 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-rm -rf feeds/luci/applications/luci-app-openclash
+
 
 # 修改网络设置
 sed -i 's/192.168.1.1/192.168.81.1/g' package/base-files/files/bin/config_generate
